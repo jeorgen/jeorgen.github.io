bccf76760d4169fa3a7fdc77e24d4f08219c6797
Jorgen Modin <jorgen@webworks.se>
2018-08-17 12:10:01 +0200

    test is now working, but test fauiling, so to speak

--- test/nmtest.js
+++ test/nmtest.js
@@ -1,34 +1,36 @@
 const Nm = require('nightmare')
-const mocha = require('mocha')
-const expect = mocha.expect
 
-// nm
-//   .goto(`file://${__dirname}/index.html`)
-//   .evaluate(() => logResult('foo'))
-//   .end()
-//   .then(console.log)
-//   .catch(error => {
-//     console.error('Search failed:', error)
-//   })
+const chai = require('chai')
+const expect = chai.expect
+
 console.log(__dirname)
 
-      const clearTextPassword = 'masonit'
-      const hexNonce = '5c62d7bff847d34e183d9bbf'
-      const result = 'e60ae5989e59aefe77192e258aa93a68cd20544705ada942c862dc9c392bf203'
-      const nm = Nm()
-      const keyStretchFactor = {
-        "N": 16**2,
-        "r": 8,
-        "p": 1,
-        "dkLen": 32,
-        "encoding": "hex"
-      }
-      Promise.resolve(1).then(()=>console.log('pwd: '+clearTextPassword))
-      nm
+const clearTextPassword = 'masonit'
+const hexNonce = '6099923682d2be5e3c6a759b'
+const result = '68ba0ece0ae313e43919a8cced68d634b9d0942188204786839e28f0662dea40'
+const nm = Nm()
+const keyStretchFactor = {
+  "N": 16 ** 2,
+  "r": 8,
+  "p": 1,
+  "dkLen": 32,
+  "encoding": "hex"
+}
+// Promise.resolve(1).then(() => console.log('pwd: ' + clearTextPassword))
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      // this.timeout('1s')
+      return nm
         .goto(`file://${__dirname}/../index.html`)
-        .evaluate((clearTextPassword, hexNonce, keyStretchFactor) => {return stretch(clearTextPassword, hexNonce, keyStretchFactor)}, clearTextPassword, hexNonce, keyStretchFactor)
+        .evaluate((clearTextPassword, hexNonce, keyStretchFactor) => {
+          return stretch(clearTextPassword, hexNonce, keyStretchFactor)
+        }, clearTextPassword, hexNonce, keyStretchFactor)
         .end()
-        .then(console.log)
-        .catch(error => {
-          console.error('Test failed:', error)
-    })
+        .then(res => {
+          console.log(res)
+          return expect(res.derivedKey).to.equal(result)
+        })
+    });
+  });
+});
