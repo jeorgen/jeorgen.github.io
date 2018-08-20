6742169560c8c68b7521a0aabde391ede123fe68
Jorgen Modin <jorgen@webworks.se>
2018-08-20 22:40:03 +0200

    Actually working test

--- test/pupp.js
+++ test/pupp.js
@@ -8,35 +8,57 @@ const assert = chai.assert
 before(async () => {
   browser = await puppeteer.launch()
   page = await browser.newPage()
+  // Ok any dialog
+  page.on("dialog", (dialog) => {
+    console.log(`Dialog has arrived and will be Ok'ed: ${dialog.message()}`);
+    dialog.accept();
+  })
 })
 
-// Start a test suite with two tests.
-describe('Roundtrip create site, backup and restore', () => {
-  it('Roundtrip create site, backup and restore', async () => {
+describe('Roundtripping end to end on UI level', () => {
+  it('Roundtrip set password, create site, backup and restore', async () => {
 
-    // Set the view port size so we can "see" the whole page
     await page.goto(`file://${__dirname}/../index.html`)
+    // Open the credential section so it's visible
     await page.click('#credentials')
+    // Let's get a snapshot of what the backup data looks like before any password is set
     const noBackupText = await page.$eval('#encrypted-backup', el => el.value)
-    console.log(noBackupText)
+    // console.log(noBackupText)
+    // Change password
     await page.type('#new-password', 'masonit')
     await page.click('#change-credentials')
     await page.click('#new-site')
-    // const watchDog = page.waitForFunction(`$(#nonce).value !=== ${oldNonce}`);
+    // Keystretching and storing is ready when the backup data has changed,
+    // since a new nonce is generated for each password change
     await page.waitForFunction(noBackupText => document.querySelector('#encrypted-backup').value !== noBackupText, {polling:200}, noBackupText);
+    // Let's snapshot the empty backup data
     const emptyBackup = await page.$eval('#encrypted-backup', el => el.value)
-
+    // Let's store a new site "lucky cat" with seed "5478923"
     await page.type('#title', 'lucky cat')
     await page.type('#seed', '5478923')
     await page.click('#save')
+    // We wait for the backup to contain the newly stored site
     await page.waitForFunction(emptyBackup => document.querySelector('#encrypted-backup').value !== emptyBackup, {polling:200}, emptyBackup);
+    // Now the backup containing our site is ready
     const actualBackup = await page.$eval('#encrypted-backup', el => el.value)
-    await page.evaluate(actualBackup => {document.querySelector('#restore').value = actualBackup}, actualBackup)
+    // Time to delete the site
+    await page.click('#delete-Title--lucky-cat')
+    // Wait for the delete to have come through
+    await page.waitForFunction(actualBackup => document.querySelector('#encrypted-backup').value !== actualBackup, {polling:200}, actualBackup)
+    // Snapshot the new empty backup
+    const newEmptyBackup = await page.$eval('#encrypted-backup', el => el.value)
+    // Let's restore the backup
+    // Open the section, so clicking works
     await page.click('#backup-restore')
-    const backup = await page.$eval('#encrypted-backup', el => el.value)
-    // Assert the search input is there
+    // Put the JSON backup into the field
+    await page.evaluate(actualBackup => {document.querySelector('#restore').value = actualBackup}, actualBackup)
+    // Restore
+    await page.click('#restore-backup')
+    // Wait for the restore to be ready
+    await page.waitForFunction(newEmptyBackup => document.querySelector('#encrypted-backup').value !== newEmptyBackup, {polling:200}, newEmptyBackup)
+    // Check if the site is back, by seeing if its delete button is back
     const searchInput = await page.$('#delete-Title--lucky-cat')
-    assert.ok(searchInput)
+    assert.ok(searchInput, "Site is back")
   }).timeout(20000)
 
   // it('shows search results after search input', async () => {
